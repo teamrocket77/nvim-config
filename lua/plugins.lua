167ed1644ca72ffa3ea9ace06f098c2af419efda
Vincent Cradler <vincentcradler@corvi.local>
2023-03-24 12:47:54 -0400

    new nvim config minor changes

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -4,27 +4,16 @@ if (not status) then
     return
 end
 
--- vim.cmd [[packadd packer.nvim]]
--- vim.cmd [[set background=light]]
 vim.cmd [[ colorscheme gruvbox ]]
--- vim.cmd [[ autocmd VimEnter * hi Normal ctermbg=none ]]
 vim.cmd [[ autocmd vimenter * ++nested colorscheme gruvbox ]]
 vim.cmd [[set winheight=10]]
 vim.cmd [[set winminheight=10]]
 
--- vim.cmd [[colorscheme tokyonight-night]]
--- vim.cmd [[colorscheme solarized]]
- -- "https://github.com/preservim/nerdtree"
 packer.startup(function(use)
-  -- path for confg ~/.cache/nvim/packer.nvim/
-  -- for PackerSnapshot
-  -- should create function that runs snapsot with date-time before running PackerSync
   use 'wbthomason/packer.nvim'
 
   use { 'L3MON4D3/LuaSnip' }
-
-  use {
-    'habamax/vim-rst',
+  use { 'habamax/vim-rst',
     ft = {'rst'},
   }
 
@@ -33,17 +22,20 @@ packer.startup(function(use)
     requires = { 'nvim-tree/nvim-web-devicons', opt = true }
   }
 
+  -- very heavy not alwyas necessary
+  -- not sure if this is needed, #TODO possibly remove
   use { 'nvim-tree/nvim-web-devicons'}
+  -- for automatic linter setup
   -- use 'onsails/lspkind-nvim'
-
   use {'VonHeikemen/lsp-zero.nvim',
   branch = 'v1.x',
   requires = {
+    -- autolsp config might give error when initalizing
     {'neovim/nvim-lspconfig'},
-
     { "williamboman/mason.nvim"},
     { "williamboman/mason-lspconfig.nvim",},
 
+    -- sm3xy suggestions
     {'hrsh7th/nvim-cmp'},
     {'hrsh7th/cmp-path'},
     {'hrsh7th/cmp-buffer'},
@@ -62,11 +54,16 @@ packer.startup(function(use)
   -- use {'neoclide/coc.nvim', branch = 'master', run = 'yarn install --frozen-lockfile'}
   use {'ellisonleao/gruvbox.nvim'}
   use { 'sindrets/diffview.nvim', requires = 'nvim-lua/plenary.nvim' }
+
+  -- enable ability for dir to be searched for files
   use { "nvim-telescope/telescope.nvim", requires = {
-    { "nvim-telescope/telescope-live-grep-args.nvim" }, }, config = function()
+    { "nvim-telescope/telescope-live-grep-args.nvim" },
+    { "BurntSushi/ripgrep" },
+    	}, config = function()
       require("telescope").load_extension("live_grep_args")
     end
   }
+  -- nice debug info
   use {
     "folke/trouble.nvim",
     requires = "nvim-tree/nvim-web-devicons",
@@ -75,10 +72,8 @@ packer.startup(function(use)
     end
   }
   use { 'pixelneo/vim-python-docstring' }
-  use {
-    'heavenshell/vim-jsdoc',
-    ft = {'js', 'jsx', 'ts', 'txs'},
-    run = 'make install'
+  use { 'kkoomen/vim-doge',
+    run = ':call doge#install()'
   }
   use 'AndrewRadev/linediff.vim'
   use({
@@ -87,3 +82,9 @@ packer.startup(function(use)
 })
 end
 )
+vim.cmd([[
+  augroup packer_user_config
+    autocmd!
+    autocmd BufWritePost plugins.lua source <afile> | PackerCompile
+  augroup end
+]])
