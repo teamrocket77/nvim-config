2d5e6b50be44ec2df4188b9240107a4e1967a9cb
Vincent Cradler <vincentcradler@corvi.local>
2023-04-06 20:05:47 -0400

    added bootstrap function

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -9,6 +9,21 @@ vim.cmd [[ autocmd vimenter * ++nested colorscheme gruvbox ]]
 vim.cmd [[set winheight=10]]
 vim.cmd [[set winminheight=10]]
 
+-- 
+local ensure_packer = function()
+  local fn = vim.fn
+  local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+  if fn.empty(fn.glob(install_path)) > 0 then
+    fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+    vim.cmd [[packadd packer.nvim]]
+    return true
+  end
+  return false
+end
+
+local packer_bootstrap = ensure_packer()
+--
+
 packer.startup(function(use)
   use 'wbthomason/packer.nvim'
 
